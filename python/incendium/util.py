e0fba4d50608c1911afc9485d295b25ea6071f66
cesrom <thcesrom@gmail.com>
2020-01-22 18:01:14 -0800

    Updated validate_form error message.

--- python/incendium/util.py
+++ python/incendium/util.py
@@ -93,30 +93,25 @@ def validate_form(strings=None, numbers=None, collections=None):
         for key, value in strings.iteritems():
             if not value:
                 counter += 1
-                if counter == 1:
-                    error_message += constants.TABBED_LINE + key
-                else:
-                    error_message += constants.NEW_TABBED_LINE + key
+                error_message += (constants.TABBED_LINE + key
+                                  if counter == 1 else
+                                  constants.NEW_TABBED_LINE + key)
                 is_valid = False
     if numbers:
         for key, value in numbers.iteritems():
             if value is None or value <= 0:
                 counter += 1
-                if counter == 1:
-                    error_message += constants.TABBED_LINE + key
-                else:
-                    error_message += constants.NEW_TABBED_LINE + key
+                error_message += (constants.TABBED_LINE + key
+                                  if counter == 1 else
+                                  constants.NEW_TABBED_LINE + key)
                 is_valid = False
     if collections:
         for key, value in collections.iteritems():
             if value is None or value <= 0:
                 counter += 1
-                error_message += (constants.TABBED_LINE + key if counter == 1
-                                  else constants.NEW_TABBED_LINE + key)
-                if counter == 1:
-                    error_message += constants.TABBED_LINE + key
-                else:
-                    error_message += constants.NEW_TABBED_LINE + key
+                error_message += (constants.TABBED_LINE + key
+                                  if counter == 1 else
+                                  constants.NEW_TABBED_LINE + key)
                 is_valid = False
 
     return is_valid, error_message
