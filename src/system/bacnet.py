5d4cc9bdeb2a2e91cecc63698d96cd39c4fa6897
César Román <thecesrom@gmail.com>
2020-11-25 20:11:00 -0800

    Added bacnet module (#7)

---
+++ src/system/bacnet.py
@@ -0,0 +1,50 @@
+# Copyright (C) 2020
+# Author: Cesar Roman
+# Contact: cesar@thecesrom.dev
+"""BACnet Functions
+The following functions are used with the BACnet driver and a BACnet/IP
+device.
+"""
+
+__all__ = ['synchronizeTime', 'synchronizeTimeUtc', 'writeWithPriority']
+
+
+def synchronizeTime(deviceName):
+    """Notifies the remote device of the correct current time, which is
+    the system time (factoring in timezone and DST) of the server
+    Ignition is running on.
+
+    Args:
+        deviceName (str): The name of the configured BACnet/IP device
+            instance to write from.
+    """
+    print deviceName
+
+
+def synchronizeTimeUtc(deviceName):
+    """Notifies the remote device of the correct current time in UTC.
+
+    Args:
+        deviceName (str): The name of the configured BACnet/IP device
+            instance to write from.
+    """
+    print deviceName
+
+
+def writeWithPriority(deviceName, objectType, objectId, value, priority):
+    """Write to the Present_Value attribute of an object with a custom
+    priority level.
+
+    Args:
+        deviceName (str): The name of the configured BACnet/IP device
+            instance to write from.
+        objectType (int): The numeric id of the objectType of the object
+            instance being written to. See the objectType reference
+            table below.
+        objectId (int): The object instance number to write to.
+        value (object): The value to write. Clearing a value can be
+            accomplished by writing a None value.
+        priority (int): The priority level to write the value at. Must
+            be in the range [1...16].
+    """
+    print(deviceName, objectType, objectId, value, priority)
