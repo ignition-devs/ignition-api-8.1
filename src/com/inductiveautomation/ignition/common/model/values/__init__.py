1f17c83b144184a626d77ff8ba6c45bb2b9dc1fc
César Román <thecesrom@gmail.com>
2021-10-10 21:24:01 -0700

    feat: add fields and implement more methods

--- src/com/inductiveautomation/ignition/common/model/values/__init__.py
+++ src/com/inductiveautomation/ignition/common/model/values/__init__.py
@@ -1,12 +1,57 @@
 __all__ = ["BasicQualifiedValue", "QualityCode", "QualifiedValue"]
 
 from java.lang import Object
+from java.util import Date
 
 
 class QualityCode(Object):
     """QualityCode contains a 32-bit integer code and optionally a
     diagnostic string.
     """
+
+    Bad = None
+    Bad_AccessDenied = None
+    Bad_AggregateNotFound = None
+    Bad_DatabaseNotConnected = None
+    Bad_Disabled = None
+    Bad_Failure = None
+    Bad_GatewayCommOff = None
+    Bad_LicenseExceeded = None
+    Bad_NotConnected = None
+    Bad_NotFound = None
+    Bad_OutOfRange = 524
+    Bad_ReadOnly = None
+    Bad_Stale = None
+    Bad_ReferenceNotFound = None
+    Bad_TrialExpired = None
+    Bad_Unauthorized = None
+    Bad_Unsupported = None
+    Error = None
+    Error_Configuration = None
+    Error_CycleDetected = None
+    Error_DatabaseQuery = None
+    Error_Exception = None
+    Error_ExpressionEval = None
+    Error_Formatting = None
+    Error_InvalidPathSyntax = None
+    Error_IO = None
+    Error_ScriptEval = None
+    Error_TagExecution = None
+    Error_TimeoutExpired = None
+    Error_TypeConversion = None
+    Good = None
+    Good_Backfill = None
+    Good_Initial = None
+    Good_Overload = None
+    Good_Provisional = None
+    Good_Unspecified = None
+    Good_WritePending = None
+    Uncertain = None
+    Uncertain_LastKnownValue = None
+    Uncertain_InitialValue = None
+    Uncertain_DataSubNormal = None
+    Uncertain_EngineeringUnitsExceeded = None
+    Uncertain_IncompleteOperation = None
 
     def __init__(self, *args):
         pass
@@ -75,12 +120,44 @@ class QualifiedValue(object):
         raise NotImplementedError
 
 
-class BasicQualifiedValue(QualifiedValue):
-    def getQuality(self):
+class BasicQualifiedValue(QualifiedValue, Object):
+    quality = QualityCode.Bad_Stale
+    timestamp = Date()
+    value = None
+
+    def __init__(self, *arg):
         pass
+
+    @staticmethod
+    def fromObject(obj):
+        pass
+
+    def getQuality(self):
+        return self.quality
 
     def getTimestamp(self):
-        pass
+        return self.timestamp
 
     def getValue(self):
+        return self.value
+
+    def setQuality(self, code):
+        self.quality = code
+
+    def setTimestamp(self, timestamp):
+        self.timestamp = timestamp
+
+    def setValue(self, value):
+        self.value = value
+
+    @staticmethod
+    def unwrap(obj):
+        pass
+
+    @staticmethod
+    def updateTimestamp(copy):
+        pass
+
+    @staticmethod
+    def valueOf(obj):
         pass
