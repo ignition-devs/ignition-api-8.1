ac01e1b390bab245e3d38b9e581920c1b418061c
César Román <thecesrom@gmail.com>
2021-10-14 12:25:50 -0700

    chore(pypi): switch to static metadata

--- src/system/__version__.py
+++ src/system/__version__.py
@@ -1,14 +1,6 @@
-# coding=utf-8
-
 """Package information."""
 
-__title__ = "ignition-api"
-__description__ = "Ignition Scripting API."
-__url__ = "https://github.com/thecesrom/Ignition"
 __version__ = "8.1.10"
-__cycle__ = ".post7"
 __build__ = "2021090812"
-__author__ = "César Román"
-__author_email__ = "cesar@thecesrom.dev"
-__license__ = "MIT"
-__copyright__ = "Copyright (C) 2018-2021 César Román"
+__cycle__ = ".post7"
+__package_version__ = "{0}{1}".format(__version__, __cycle__)
