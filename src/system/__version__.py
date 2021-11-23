ca1e665585c5178cd9cac0252bd0e3c302adc282
César Román <thecesrom@gmail.com>
2021-11-23 15:53:12 -0800

    chore: release 8.1.12

--- src/system/__version__.py
+++ src/system/__version__.py
@@ -1,6 +1,6 @@
 """Package information."""
 
-__version__ = "8.1.11"
-__build__ = "2021101912"
+__version__ = "8.1.12"
+__build__ = "2021111810"
 __cycle__ = ""
 __package_version__ = "{0}{1}".format(__version__, __cycle__)
