b4b5e3b2977a1f025f9f9f07a061950faabe8c01
César Román <thecesrom@gmail.com>
2022-08-16 14:24:49 -0700

    feat(java): add functions to awt classes (#62)
    * feat(java): add functions to awt classes
    
    Signed-off-by: César Román <thecesrom@gmail.com>
    
    * fix(java): remove cyclic import
    
    Signed-off-by: César Román <thecesrom@gmail.com>
    
    * fix(java): solve pylint issues
    
    super-init-not-called
    ImageObserver not defined (removed as it created a cyclic-import)
    
    Signed-off-by: César Román <thecesrom@gmail.com>

--- src/java/awt/__init__.py
+++ src/java/awt/__init__.py
@@ -1,9 +1,3 @@
-"""Package java.awt.
-
-Contains all of the classes for creating user interfaces and for
-painting graphics and images.
-"""
-
 from __future__ import print_function
 
 __all__ = [
@@ -22,66 +16,46 @@ from java.lang import Object
 
 
 class Color(Object):
-    """The Color class is used to encapsulate colors in the default sRGB
-    color space or colors in arbitrary color spaces identified by a
-    ColorSpace.
-
-    Every color has an implicit alpha value of 1.0 or an
-    explicit one provided in the constructor. The alpha value defines
-    the transparency of a color and can be represented by a float value
-    in the range 0.0 - 1.0 or 0 - 255. An alpha value of 1.0 or 255
-    means that the color is completely opaque and an alpha value of 0 or
-    0.0 means that the color is completely transparent. When
-    constructing a Color with an explicit alpha or getting the
-    color/alpha components of a Color, the color components are never
-    premultiplied by the alpha component.
-
-    The default color space for the Java 2D(tm) API is sRGB, a proposed
-    standard RGB color space. For further information on sRGB, see
-    http://www.w3.org/pub/WWW/Graphics/Color/sRGB.html.
-    """
-
     def __init__(self, *args):
         # type: (Any) -> None
         print(args)
 
 
 class Component(Object):
-    """A component is an object having a graphical representation that
-    can be displayed on the screen and that can interact with the user.
-    """
-
-    pass
+    def imageUpdate(
+        self,
+        img,  # type: Image
+        infoflags,  # type: int
+        x,  # type: int
+        y,  # type: int
+        width,  # type: int
+        height,  # type: int
+    ):
+        # type: (...) -> bool
+        pass
 
 
 class Container(Component):
-    """A generic Abstract Window Toolkit(AWT) container object is a
-    component that can contain other AWT components.
-    """
-
     def add(self, *args):
         # type: (Any) -> Optional[Component]
         pass
 
 
 class Image(Object):
-    """The abstract class Image is the superclass of all classes that
-    represent graphical images.
+    def flush(self):
+        # type: () -> None
+        pass
 
-    The image must be obtained in a platform-specific manner.
-    """
+    def getAccelerationPriority(self):
+        # type: () -> float
+        pass
 
-    pass
+    def setAccelerationPriority(self, priority):
+        # type: (float) -> None
+        pass
 
 
 class Toolkit(Object):
-    """This class is the abstract superclass of all actual
-    implementations of the Abstract Window Toolkit.
-
-    Subclasses of the Toolkit class are used to bind the various
-    components to particular native toolkit implementations.
-    """
-
     def __init__(self):
         # type: () -> None
         pass
@@ -97,14 +71,12 @@ class Toolkit(Object):
 
 
 class Window(Container):
-    """A Window object is a top-level window with no borders and no
-    menubar.
-    """
-
-    pass
+    def __init__(self, *args):
+        # type: (Any) -> None
+        pass
 
 
 class Frame(Window):
-    """A Frame is a top-level window with a title and a border."""
-
-    pass
+    def __init__(self, *args):
+        # type: (Any) -> None
+        super(Frame, self).__init__(*args)
