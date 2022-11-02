5beaecc03a3fa35d2fe76571548dc9c846f926af
César Román <thecesrom@gmail.com>
2022-11-02 13:50:08 -0700

    refactor(org): update MathIllegalArgumentException __init__ signature
    Signature is not compatible with __new__

--- src/org/apache/commons/math3/exception/__init__.py
+++ src/org/apache/commons/math3/exception/__init__.py
@@ -18,9 +18,9 @@ class MathIllegalArgumentException(IllegalArgumentException):
     semantics of the standard IllegalArgumentException.
     """
 
-    def __init__(self, pattern, *args):
-        # type: (String, *Any) -> None
-        print(pattern, args)
+    def __init__(self, *args):
+        # type: (*Any) -> None
+        print(args)
         super(MathIllegalArgumentException, self).__init__()
 
     def getContext(self):
@@ -46,7 +46,7 @@ class DimensionMismatchException(MathIllegalNumberException):
 
     def __init__(self, *args):
         # type: (*Any) -> None
-        super(DimensionMismatchException, self).__init__("", *args)
+        super(DimensionMismatchException, self).__init__(*args)
 
     def getDimension(self):
         pass
