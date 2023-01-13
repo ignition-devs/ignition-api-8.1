243a0a7d4230275e2daba9c60f76c33f59b1cc1f
César Román <thecesrom@gmail.com>
2023-01-13 06:44:06 -0800

    feat(system): add vision module (#149)
    tox always uses isolated builds
    
    Closes: #148

---
+++ src/system/vision.py
@@ -0,0 +1,16 @@
+"""Vision Functions
+
+The following function will allow you to update your Vision Client
+project using scripting.
+"""
+
+
+def updateProject():
+    # type: () -> None
+    """Updates the Vision Client project with saved changes.
+
+    This function is intended to be used in conjunction with the "None"
+    option of Vision Project update modes in the Project Properties, and
+    the Vision Client System Tag ProjectUpdateAvailable.
+    """
+    pass
