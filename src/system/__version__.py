33b7376a7d738eec96c62c61acc432d82c310a44
César Román <thecesrom@gmail.com>
2023-02-21 21:53:34 -0800

    chore(release): prepare for v8.1.25.post1

--- src/system/__version__.py
+++ src/system/__version__.py
@@ -1,6 +1,6 @@
 """Package information."""
 
-__version__ = "8.1.24"
-__build__ = "2023011208"
+__version__ = "8.1.25"
+__build__ = "2023021409"
 __cycle__ = ".post1"
 __package_version__ = "{0}{1}".format(__version__, __cycle__)  # type: str
